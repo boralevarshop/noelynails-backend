be4b42ec88e609504985f06e9d6ca5231af2caa0
Bora Levar Shop <boralevarshop@gmail.com>
2025-11-21 10:32:04 -0300

    Fix: Habilitando CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilita o CORS para que o Frontend (em outro domínio) consiga acessar
+  app.enableCors({
+    origin: true, // Em produção, trocaremos isso pelo domínio exato
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Pega a porta do ambiente ou usa 3000
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
